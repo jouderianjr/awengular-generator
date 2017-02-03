2c4dab265281c2d1d55a4618248f3553d69ca3b4
Jouderian Nobre <jouderian.nobre@ifactory.com.br>
2017-02-03 18:26:49 -0300

    Working in progress

--- awengen.js
+++ awengen.js
@@ -4,7 +4,7 @@
 const cli             = require('commander')
 const fileCreator     = require('./file-creator')
 const fileGenerator   = require('./file-generator')
-const moduleGenerator = require('./moduleGenerator')
+const moduleGenerator = require('./module-generator')
 
 cli
     .version('0.0.1')
@@ -15,8 +15,8 @@ cli
     .command('service <name> <moduleName>', 'Generate an angular service')
     .on('service', (data) => {fileGenerator.generate(data, 'Service')})
 
-    .command('module [name] <moduleName>', 'Generate an angular module')
-    .on('module', moduleGenerator.generate(data))
+    .command('module <name> <moduleName>', 'Generate an angular module')
+    .on('module', moduleGenerator.generate)
     .parse(process.argv)
 
 module.exports = cli
--- module-generator.js
+++ module-generator.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const fs            = require('fs')
+const fileGenerator = require('./file-generator')
+const utils         = require('./utils')
+
+function generate(args) {
+    let name       = args[0]
+    let moduleName = args[1]
+
+    return new Promise((resolve, reject) => {
+        //Should create the folder, controller, service and template
+        fs.mkdir(utils.hiphenizeStr(name), (err, data) => {
+            if(err && err !== '')
+            console.log('err')
+            console.log(err)
+            console.log('data')
+
+            console.log(data)
+        })
+    })
+}
+
+exports.generate = generate
