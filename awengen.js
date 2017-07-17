285ba9e2b1410990bea12e171cdf068c709fa77b
Jouderian Nobre <jouderian.nobre@ifactory.com.br>
2017-07-17 15:54:28 -0300

    Add more feedback messages

--- awengen.js
+++ awengen.js
@@ -5,8 +5,12 @@ const cli             = require('yargs')
 const fileCreator     = require('./file-creator')
 const fileGenerator   = require('./file-generator')
 const moduleGenerator = require('./module-generator')
+const chalk           = require('chalk')
+const figlet          = require('figlet')
 
-cli
+const validCommands = ['controller', 'service', 'module']
+
+const argv = cli
     .command(
         'controller <name> <moduleName> [folder]',
         'Generate an angular controller',
@@ -27,3 +31,24 @@ cli
     )
     .help()
     .argv
+
+
+if (argv._.length === 0) { welcomeMessage() }
+
+if( argv._.length > 0 && isNotValidCommand(argv._[0])) { showInvalidCommandMessage() }
+
+function isNotValidCommand(command) {
+    return !validCommands.some( valid => command === valid)
+}
+
+function welcomeMessage() {
+    console.log(chalk.yellow(figlet.textSync('Awengen')))
+    console.log(chalk.blue('Type awengen --help to see all options'))
+}
+
+
+function showInvalidCommandMessage() {
+    console.log(chalk.yellow(figlet.textSync('Awengen')))
+    console.log(chalk.red('Invalid command'))
+    console.log(chalk.blue('Type awengen --help to see all options'))
+}
